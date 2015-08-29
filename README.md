f41664f01768103debe85b6c9c015a96f9d03c7a
Rajesh Subramanian <rsubramanian@resource.com>
2015-08-29 16:06:35 -0400

    Added a navigation drawer.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Android - Navigation Drawer Example
+
+[Getting started](https://developer.android.com/training/implementing-navigation/nav-drawer.html)
+[Android Design Support Library](http://android-developers.blogspot.com/2015/05/android-design-support-library.html)
--- app/src/main/java/com/rsubramanian/navigationexample/MainActivity.java
+++ app/src/main/java/com/rsubramanian/navigationexample/MainActivity.java
@@ -1,16 +1,41 @@
 package com.rsubramanian.navigationexample;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+	private static final String LOG_TAG = MainActivity.class.getName();
+
+	private DrawerLayout drawerLayout;
+
+	private NavigationView navigationView;
+
+	private Toolbar toolbar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+
+		navigationView = (NavigationView) findViewById(R.id.navigationView);
+		navigationView.setNavigationItemSelectedListener(this);
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+		ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_open, R.string.navigation_close);
+		drawerToggle.syncState();
+		drawerLayout.setDrawerListener(drawerToggle);
 	}
 
 	@Override
@@ -33,5 +58,23 @@ public class MainActivity extends AppCompatActivity {
 		}
 
 		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+		Log.d(LOG_TAG, "onNavigationItemSelected: " + menuItem.getTitle());
+		switch (menuItem.getItemId()) {
+
+			case R.id.menuItem1: {
+				drawerLayout.closeDrawer(GravityCompat.START);
+				break;
+			}
+			case R.id.menuItem2: {
+				drawerLayout.closeDrawer(GravityCompat.START);
+				break;
+			}
+		}
+		return false;
 	}
 }
