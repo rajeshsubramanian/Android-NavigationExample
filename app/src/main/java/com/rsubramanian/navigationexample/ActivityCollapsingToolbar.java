58dc603144698fa64122583baf830d8be4cc9d5c
Rajesh Subramanian <rsubramanian@resource.com>
2015-11-03 21:29:17 -0500

    Added a collapsing toolbar activity.

---
+++ app/src/main/java/com/rsubramanian/navigationexample/ActivityCollapsingToolbar.java
@@ -0,0 +1,45 @@
+package com.rsubramanian.navigationexample;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ActivityCollapsingToolbar extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_collapsing_toolbar);
+		initNavigation();
+
+		((Toolbar) findViewById(R.id.toolbar)).setTitle(R.string.title_activity_collapsing_toolbar);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_activity_two, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void handleNavigationMenu() {
+		getNavigationView().getMenu().getItem(2).setVisible(false);
+	}
+}
